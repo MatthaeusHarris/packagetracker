8ebf9e53a89322734d7530662d947746ae652649
Matt Harris <matthaeus.harris@gmail.com>
2015-03-11 00:07:02 -0700

    Minor tweaks

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('home');
+    res.render('about');
 });
 
 router.get('/about', function(req, res) {
