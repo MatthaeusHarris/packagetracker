e4bb198347264f3cd23d575b1b2e8070bd130773
Matt Harris <matthaeus.harris@gmail.com>
2015-03-20 02:21:49 -0700

    Add git log to front page

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@
  */
 var express = require('express');
 var router = express.Router();
+var gitLog = require('../lib/gitlog');
 
 router.get('/', function(req, res) {
-    res.render('about');
+    res.render('about', {log: gitLog.log});
 });
 
 router.get('/about', function(req, res) {
-    res.render('about');
+    console.log(gitLog);
+    res.render('about', {log: gitLog.log});
 });
 
 module.exports = router;
