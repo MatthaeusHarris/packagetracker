497f98d908d694176e2990cddeff81b15332dd96
Matt Harris <matthaeus.harris@gmail.com>
2015-04-10 19:17:35 -0700

    Add logging to CI hook

--- routes/ci.js
+++ routes/ci.js
@@ -6,6 +6,7 @@ var githubApi = require('node-github');
 var express = require('express');
 var router = express.Router();
 var gitlog = require('../lib/gitlog');
+var child_process = require('child_process');
 
 var githubInstance = new githubApi({
     version: '3.0.0'
@@ -22,14 +23,22 @@ router.get('/ci/hook/update', function(req, res) {
 
         if (data[0].sha !== gitlog.log[0].hash) {
             // We need to update
+            console.log("CI update hook triggered.  Fetching updates");
             child_process.exec('git pull', function(err, stdout, stderr) {
                 if (err) {
+                    console.log(stderr);
                     return res.render('500', [err, stdout, stderr].join('\n'));
                 }
+                console.log(stdout);
+                console.log(stderr);
+                console.log("Installing any new libraries");
                 child_process.exec('npm install', function(err, stdout, stderr) {
                     if (err) {
+                        console.log(stderr);
                         return res.render('500', [err, stdout, stderr].join('\n'))
                     }
+                    console.log(stdout);
+                    console.error(stderr);
                     process.nextTick(function() {
                         console.log("Process exiting to restart after CI chain trigger");
                         process.exit(0);
